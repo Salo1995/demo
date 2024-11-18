9404b674af744e410410a798ac56e777a78494ee
Salo1995 <102559544+Salo1995@users.noreply.github.com>
2024-11-18 15:44:05 -0300

    cambios para disponibilidad base de datos

--- src/main/java/com/juegodemesa/controller/Juegocontroller.java
+++ src/main/java/com/juegodemesa/controller/Juegocontroller.java
@@ -7,38 +7,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 import com.juegodemesa.model.Juego;
 import com.juegodemesa.service.JuegoService;
 
-
 @RestController
-@RequestMapping ("/api/juego")
+@RequestMapping("/api/juego")
 public class Juegocontroller {
 
     @Autowired
     private JuegoService juegoService;
 
+    // Método para listar todos los juegos
+    @GetMapping
     public List<Juego> listaJuego() {
         return juegoService.listarJuego(); 
     }
 
+    // Método para crear un nuevo juego
     @PostMapping
     public Juego crearJuego(@RequestBody Juego juego) {
         return juegoService.guardarJuego(juego);
     }
-    
-    
+
+    // Método para obtener un juego por ID
     @GetMapping("/{id}")
     public ResponseEntity<Juego> obtenerJuego(@PathVariable Long id) {
         Optional<Juego> juego = juegoService.obtenerJuego(id);
         return juego.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    // Método para eliminar un juego por ID
     @DeleteMapping("/{id}")
-    public void eliminarJuego (@PathVariable Long id) {
+    public void eliminarJuego(@PathVariable Long id) {
         juegoService.eliminarJuego(id);
-
     }
-
 }
